071524c5ce661cd7df4eddc7e7e7de72ee24c5e0
dschnoor2 <59981825+dschnoor2@users.noreply.github.com>
2020-04-05 16:41:51 -0500

    Program Update

---
+++ PersonalProjectLab/PersonalProjectLab/PrintCostEstimatior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace PersonalProjectLab
+{
+    public class PrintCostEstimator
+    {
+        public PrintCostEstimator()
+        {
+        }
+
+        public decimal CalculatingMaterialCost ( int filamentAmountNeeded, int rollCost, int rollSize)
+        {
+            decimal materialCost = 0.00m;
+
+            materialCost = (decimal)filamentAmountNeeded * rollCost / rollSize;
+
+            return materialCost;
+        }
+        public decimal CalculatingMachineCost(int machineHour)
+        {
+            decimal kiloWattHourPrinter = 0.20m;
+            decimal kiloWattHourElectricity = 0.12m;
+
+            decimal machineCost = 0.00m;
+            machineCost = kiloWattHourElectricity * kiloWattHourPrinter * machineHour;
+                        
+            return machineCost;
+        }
+        public decimal CalculatingManHoursCost(int manHours, int costPerHour)
+        {
+            decimal manHoursCost = 0.00m;
+            manHoursCost = (decimal) manHours * costPerHour;
+
+            return manHoursCost;
+        }   
+        
+    }
+}
---
+++ PersonalProjectLab/PersonalProjectLabTests/PriceCostTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonalProjectLab;
+
+namespace PersonalProjectLabTests
+{
+    [TestClass]
+    public class PriceCostTest
+    {
+        [TestMethod]
+        public void PriceCosts_MaterialCostTest()
+        {
+            //arrange
+            PrintCostEstimator stats = new PrintCostEstimator();
+
+            //acting
+            decimal materialCosts = stats.CalculatingMaterialCost(25, 50, 1000);
+
+            //asserting
+            Assert.AreEqual(1.25m, materialCosts);
+        }
+        [TestMethod]
+        public void PriceCosts_ManCostTest()
+        {
+            //arrange
+            PrintCostEstimator stats = new PrintCostEstimator();
+
+            //acting
+            decimal manHoursCost = stats.CalculatingManHoursCost(20, 5);
+
+            //asserting
+            Assert.AreEqual(100.00m, manHoursCost);
+        }
+    }
+}
