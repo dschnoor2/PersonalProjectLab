ab6c992a6590f20a4905a1b1b6260a6b9b7376f0
dschnoor2 <59981825+dschnoor2@users.noreply.github.com>
2020-04-10 17:18:59 -0500

    Code Cleaned
    Removal of Redundant Code
    Addition of tabbed outputs

--- PersonalProjectLab/PersonalProjectLab/PrintCostEstimatior.cs
+++ PersonalProjectLab/PersonalProjectLab/PrintCostEstimatior.cs
@@ -10,13 +10,11 @@ namespace PersonalProjectLab
 
         public decimal CalculatingMaterialCost ( int filamentAmountNeeded, int rollCost, int rollSize)
         {
-           //Material Cost is determined by (cost of the Roll / Roll Size) to get price per gram.
-           //Price per gram multipled by amount of material used.
-           //Material Cost mulitplied by 2 to account for printing errors
-            decimal materialCost = 0.00m;
 
-            materialCost = (decimal)filamentAmountNeeded * rollCost / rollSize *2;
-
+            //Material Cost is determined by (cost of the Roll / Roll Size) to get price per gram.
+            //Price per gram multipled by amount of material used.
+            //Material Cost mulitplied by 2 to account for printing errors
+            decimal materialCost = filamentAmountNeeded * rollCost / rollSize * 2;
             return materialCost;
         }
 
@@ -26,26 +24,21 @@ namespace PersonalProjectLab
             //KiloWatt Hour Printer Usage and KiloWatt for Electricity Charge are determined by current averages found online.
             decimal kiloWattHourPrinter = 0.20m;
             decimal kiloWattHourElectricity = 0.12m;
-
-            decimal electricalCost = 0.00m;
-            electricalCost = kiloWattHourElectricity * kiloWattHourPrinter * machineHour * machineCostPerHour;
+            decimal electricalCost = kiloWattHourElectricity * kiloWattHourPrinter * machineHour * machineCostPerHour;
 
             //Maintenance = man time to maintain the machine * cost of machine/estimated liftime(in hrs) * machine hours
-            decimal maintenance =0.00m;
-            maintenance = (decimal)(.5 * 300 / 10000 * machineHour * 20);
+            decimal maintenance = (decimal)(.5 * 300 / 10000 * machineHour * 20);
 
             //Sum of 2 costs = What it cost to run the machine.
-            decimal machineCost = 0.00m;
-            machineCost = maintenance + electricalCost;
-                        
+            decimal machineCost = maintenance + electricalCost;
+
             return machineCost;
         }
 
         public decimal CalculatingManHoursCost(int manHours, int manCostPerHour)
         {
             //Man Hours Cost = Estimated man hours (including file creating, set up, physical set up) * cost per man hour
-            decimal manHoursCost = 0.00m;
-            manHoursCost = (decimal) manHours * manCostPerHour;
+            decimal manHoursCost = manHours * manCostPerHour;
 
             return manHoursCost;
         }   
@@ -53,9 +46,9 @@ namespace PersonalProjectLab
         public decimal CalculatingTotalSalesCost(decimal materialCost, decimal machineCost, decimal manHoursCost)
         {
             decimal minimumSale = 25.00m;
-            decimal TotalCostPrint = 0.00m;
-            TotalCostPrint = (materialCost + machineCost + manHoursCost) * (decimal)(0.07) + (materialCost + machineCost + manHoursCost);
-           
+            decimal tax = (materialCost + machineCost + manHoursCost) *(decimal).07;
+            decimal TotalCostPrint = materialCost + machineCost + manHoursCost + tax;
+
             if (TotalCostPrint < minimumSale)
             {
                 return minimumSale;
--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
-using System.IO; 
+using System.IO;
+using System.Reflection.Metadata;
 
 namespace PersonalProjectLab
 {
@@ -16,70 +17,55 @@ namespace PersonalProjectLab
 
             while (userConintue) 
             {
-                //Prompt User to enter Filament Information including Roll Cost, Roll Size, and Estimated amount needed
-
-
-                string materialInput = "";
-                int filamentAmountNeeded = 0;
-                int rollCost = 0;
-                int rollSize = 0;
-
                 Console.WriteLine("Enter the Estimated amount of filament needed in grams");
                 Console.WriteLine("(This Includeds Supports, Rafts, Brim)");
-                materialInput = Console.ReadLine();
-                filamentAmountNeeded = int.Parse(materialInput);
+                
+                //Prompt User to enter Filament Information including Roll Cost, Roll Size, and Estimated amount needed
+                string materialInput = Console.ReadLine();
+                int filamentAmountNeeded = int.Parse(materialInput);
 
                 Console.WriteLine("Enter the Cost of the Roll");
                 materialInput = Console.ReadLine();
-                rollCost = int.Parse(materialInput);
+                int rollCost = int.Parse(materialInput);
 
                 Console.WriteLine("Enter the Size of the Roll in grams");
                 Console.WriteLine("(This value is typically 500g or 1000g)");
                 materialInput = Console.ReadLine();
-                rollSize = int.Parse(materialInput);
+                int rollSize = int.Parse(materialInput);
 
                 //Calculate Material Costs
                 PrintCostEstimator stats = new PrintCostEstimator();
 
                 decimal materialCost = stats.CalculatingMaterialCost(filamentAmountNeeded, rollCost, rollSize);
-
-                //Prompt User to enter Machine Cost including estimated Machine Run Time and Charge per Hour for Machine
-                string machineInputs = "";
-                int machineHours = 0;
-                int machineCostPerHour = 0;
-
                 Console.WriteLine("");
                 Console.WriteLine("Enter the Estimated Print Time in Hours");
-                machineInputs = Console.ReadLine();
-                machineHours = int.Parse(machineInputs);
+                //Prompt User to enter Machine Cost including estimated Machine Run Time and Charge per Hour for Machine
+                string machineInputs = Console.ReadLine();
+                int machineHours = int.Parse(machineInputs);
 
                 Console.WriteLine("Enter Charge to Run Machine");
                 Console.WriteLine("(Typically 0 to 5 depending on the desired level of detail)");
                 machineInputs = Console.ReadLine();
-                machineCostPerHour = int.Parse(machineInputs);
+                int machineCostPerHour = int.Parse(machineInputs);
 
                 //Calculate Machine Costs               
-                decimal machineCost = stats.CalculatingMachineCost(machineHours, machineCostPerHour);                            
-
-                //Prompt User to enter Man Hour Cost including estimated Man Hours needed and Cost per Man Hour
-                string manInputs = "";
-                int manHours = 0;
-                int manCostPerHour = 0;
-                
+                decimal machineCost = stats.CalculatingMachineCost(machineHours, machineCostPerHour);
                 Console.WriteLine("");
                 Console.WriteLine("Enter Estimated Man Hours");
                 Console.WriteLine("(This includes File Setup, File Creation, and Final Cleaning)");
-                manInputs = Console.ReadLine();
-                manHours = int.Parse(manInputs);
+                //Prompt User to enter Man Hour Cost including estimated Man Hours needed and Cost per Man Hour
+                string manInputs = Console.ReadLine();
+                int manHours = int.Parse(manInputs);
 
                 Console.WriteLine("Enter Cost per Man Hour");
                 manInputs = Console.ReadLine();
-                manCostPerHour = int.Parse(manInputs);
+                int manCostPerHour = int.Parse(manInputs);
 
                 //Calculate Man Hours Cost 
                 decimal manHoursCost = stats.CalculatingManHoursCost(manHours, manCostPerHour);
 
-
+                //Calculate Taxed Amount
+                decimal tax = (materialCost + machineCost + manHoursCost) * (decimal)(0.07);
                 //Calculate Sale Costs Estimation                
                 decimal totalSalesCost = stats.CalculatingTotalSalesCost(materialCost, machineCost, manHoursCost);
 
@@ -94,24 +80,28 @@ namespace PersonalProjectLab
 
                     //Print Calculated All Values to Console
                     Console.WriteLine("");//Readablility
-                    Console.WriteLine("File Description: " + printDescription);
-                    Console.WriteLine("Material: " + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                    Console.WriteLine("Machine: " + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                    Console.WriteLine("Man Hours: " + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                    Console.WriteLine("Tax: 7%");
-                    Console.WriteLine("Total Cost: " + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("File Description: \t" + printDescription);
+                    Console.WriteLine("Material: \t\t" + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Machine: \t\t" + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Man Hours: \t\t" + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Tax: \t\t\t" + tax.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("********************************");
+                    Console.WriteLine("Total Cost: \t\t" + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
                     Console.WriteLine("");//Readability
 
                     //Create Stream Writer and save new estimated values to file                             
                     using (StreamWriter outputFile = new StreamWriter("PrintCostEstimation.txt"))
                     {
-                        outputFile.WriteLine(DateTime.Now);
-                        outputFile.WriteLine("File Description: " + printDescription);
-                        outputFile.WriteLine("Material: " + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                        outputFile.WriteLine("Machine: " + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                        outputFile.WriteLine("Man Hours: " + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                        outputFile.WriteLine("Tax: 7%");
-                        outputFile.WriteLine("Total Cost: " + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("*****3D Print Cost Estimation*****");
+                        outputFile.WriteLine("");//Readability
+                        outputFile.WriteLine("File Description: \t" + printDescription);
+                        outputFile.WriteLine("Material: \t\t" + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Machine: \t\t" + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Man Hours: \t\t" + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Tax: \t\t\t" + tax.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("********************************");
+                        outputFile.WriteLine("Total Cost: \t\t" + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Document Created on: \t" + DateTime.Now);
                     }
                 }
 
@@ -119,23 +109,26 @@ namespace PersonalProjectLab
                 {
                     //Print Calculated All Values to Console
                     Console.WriteLine("");//Readability
-                    Console.WriteLine("Material: " + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                    Console.WriteLine("Machine: " + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                    Console.WriteLine("Man Hours: " + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                    Console.WriteLine("Tax: 7%");
-                    Console.WriteLine("Total Cost: " + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Material: \t\t" + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Machine: \t\t" + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Man Hours: \t\t" + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                    Console.WriteLine("Tax: \t\t\t" + tax.ToString("C", CultureInfo.GetCultureInfo("en-US"))); ;
+                    Console.WriteLine("********************************");
+                    Console.WriteLine("Total Cost: \t\t" + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
                     Console.WriteLine("");//Readability
 
                     //Create Stream Writer and save new estimated values to file                             
                     using (StreamWriter outputFile = new StreamWriter("PrintCostEstimation.txt"))
                     {
-                        outputFile.WriteLine(DateTime.Now);
+                        outputFile.WriteLine("*****3D Print Cost Estimation*****");
                         outputFile.WriteLine("");//Readability
-                        outputFile.WriteLine("Material: " + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                        outputFile.WriteLine("Machine: " + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                        outputFile.WriteLine("Man Hours: " + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-                        outputFile.WriteLine("Tax: 7%");
-                        outputFile.WriteLine("Total Cost: " + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Material: \t\t" + materialCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Machine: \t\t" + machineCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Man Hours: \t\t" + manHoursCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Tax: \t\t\t" + tax.ToString("C", CultureInfo.GetCultureInfo("en-US"))); ;
+                        outputFile.WriteLine("********************************");
+                        outputFile.WriteLine("Total Cost: \t\t" + totalSalesCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                        outputFile.WriteLine("Document Created on: \t" + DateTime.Now);
                     }
                 }
                 //Ask User if they wish to continue
@@ -145,7 +138,6 @@ namespace PersonalProjectLab
                 //If yes, Return to start of Program
                 if (userAnswer == "2")
                 {
-                    //Else, End Program
                     userConintue = false;
                 }                                   
             }
